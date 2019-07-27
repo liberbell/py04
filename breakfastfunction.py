23a58d31b9373ee2970258b73a00d5bb157339ee
Liberbell <liberbell@gmail.com>
2019-07-27 11:12:07 +0900

    breakfastfunction2

---
+++ breakfastfunction.py
@@ -0,0 +1,9 @@
+def mix_and_cook:
+    print('Mixing the ingredients')
+    print('Greasing the frying pan')
+    print('Cooking the first side')
+
+def make_omlette():
+    mix_and_cook()
+    omlette = 'a tasty omlette'
+    return omlette
