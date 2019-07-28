91d999aab69a9c3fd3f6a0dd050f109c43c0aebe
Liberbell <liberbell@gmail.com>
2019-07-29 07:45:13 +0900

    urllibstart3

--- urllibstart.py
+++ urllibstart.py
@@ -4,5 +4,8 @@ def main:
     url = "https://httpbin.org/xml"
 
     result = urllib.result.urlopen(url)
+    print("Result code: {0}".format(result.satus))
 
     print("Headers:----------")
+
+    print("Returned data:----")
