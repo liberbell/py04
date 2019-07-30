b4a2f20620f957efd8ac0e7bb87e5ddb81c53858
Liberbell <liberbell@gmail.com>
2019-07-30 12:42:33 +0900

    urlliberr4

--- urlliberr.py
+++ urlliberr.py
@@ -2,8 +2,8 @@ import urllib.request
 
 def main():
     # url = "https://no-such-server.org/html"
-    # url = "https://httpbin.org/status/404"
-    url = "https://httpbin.org/html"
+    url = "https://httpbin.org/status/404"
+    # url = "https://httpbin.org/html"
 
     result = urllib.request.urlopen(url)
     print("Result code: {0}".format(result.status))
