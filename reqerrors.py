549317a828bb7abb8a485c8fec801f3ffcad8cf9
Liberbell <liberbell@gmail.com>
2019-07-31 15:47:02 +0900

    reqerrors3

--- reqerrors.py
+++ reqerrors.py
@@ -4,3 +4,8 @@ def main():
     url = 'https://httpbin.org/status/404'
     result = requests.get(url)
     printResults(result)
+
+
+def printResults(resData):
+    print('Result data: {0}'.format(resData.status_code))
+    print('\n')
