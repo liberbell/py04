2f26abe15e484d4ab98d6e4a2ce12a5776edb665
Liberbell <liberbell@gmail.com>
2019-08-01 07:36:09 +0900

    jsonserial2

---
+++ jsonserial.py
@@ -0,0 +1,13 @@
+import json
+
+def main():
+    pythonData = {
+        "sandwich": "Reuben",
+        "toasted": True,
+        "toppings": [
+            "Thousand island Dressing",
+            "Sauerkraut",
+            "Pickles"
+        ],
+        "price": 8.99
+    }
