dd062cc37b7644e1ec9ddf1e98c659128dec0aee
Liberbell <liberbell@gmail.com>
2019-08-01 07:32:44 +0900

    jsonparse3

--- jsonparse.py
+++ jsonparse.py
@@ -10,4 +10,15 @@ def main():
             "Pickles"
         ],
         "price": 8.99
-    }
+    }'''
+
+    data = json.loads(jsonStr)
+    print('Sandwich: ' + data['sandwich'])
+    if (data[toasted]):
+        print('And it`s toasted.')
+
+    for topping in data['toppings']:
+        print('Topping: ' + topping)
+
+if __name__ == '__main__':
+    main()
