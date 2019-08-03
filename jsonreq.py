48941477bd8d8e0bc0388c09b36d426c911e0e63
Liberbell <liberbell@gmail.com>
2019-08-03 12:15:48 +0900

    jsonreq2

--- jsonreq.py
+++ jsonreq.py
@@ -10,6 +10,7 @@ def main():
 
     print(list(dataobj.keys()))
     print(dataobj['slideshow']['title'])
+    print('There are {0} slides.'.format(len(dataobj['slideshow']['slides'])))
 
 if __name__ == '__main__':
     main()
