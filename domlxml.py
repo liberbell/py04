d274d185c57fb358611d4412ce138ef372d56ecc
Liberbell <liberbell@gmail.com>
2019-08-05 07:39:14 +0900

    domlxml4

--- domlxml.py
+++ domlxml.py
@@ -5,6 +5,9 @@ def main():
     url = 'https://httpbin.org/xml'
     result = requests.get(url)
 
+    doc = etree.fromstring(result.content)
+    print(result.text)
+
 
 if __name__ == '__main__':
     main()
